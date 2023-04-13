6a45f038b7635d39baa0c2a0d0932339efc354da
Negative-3U <31720521+Negative-3U@users.noreply.github.com>
2023-04-13 18:05:45 +0800

    Upload script
    lol

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # AffOptimizer
  This script will "optimize" your .aff file to reduce the size
+
+## This script will:
+
+1. Replace all `arctap` with the alias `at`.
+2. Remove all unnecessary zeros in decimals.
+3. Use LF line breaks and remove all unnecessary newlines.
+4. Remove all useless content in header.
+
+## Usage
+
+```
+python3 aff_optimizer.py <in.aff> <out.aff>
+```
+
+## Testing result
+
+ Typically, this script reduces the .aff file size by about 15%:
+
+```
+ω4            2.aff: 173KB -> 142KB
+Pentiment     3.aff: 106KB -> 90.3KB
+Testify       3.aff: 101KB -> 87.7KB
+Callima Karma 2.aff: 91.7KB -> 78.1KB
+
+and...
+10MB.aff: 10MB -> 8.31MB
+```
+
---
+++ aff_optimizer.py
@@ -0,0 +1,28 @@
+import sys
+import re
+
+def optimize(s: str) -> str:
+  i = s.find("\n-\n")
+  ao = re.search("AudioOffset *: *([0-9]*) *", s[:i])
+  tpdf = re.search("TimingPointDensityFactor *: *([0-9.]*) *", s[:i])
+  header = ("AudioOffset:{}".format(ao.groups()[0]) + 
+            ("\nTimingPointDensityFactor:{}".format(tpdf.groups()[0]) if tpdf else "") + 
+            "\n-\n")
+  content = re.sub("(?<=[,-])0?([1-9]?[0-9]*\.[1-9]*)0*", lambda a: a.groups()[0], s[i+3:])
+  content = re.sub("arctap\(", "at(", content)
+  content = re.sub("\s", "", content)
+  return header + content
+
+def main(args):
+  if len(args) != 3:
+    print("Usage: aff_optimizer.py <input.aff> <output.aff>")
+    return 1
+  (_, fi, fo) = args
+  with open(fi, mode='r') as i:
+    aff = i.read()
+    with open(fo, mode='w', newline='\n') as o:
+      o.write(optimize(aff))
+  return 0
+
+if __name__ == "__main__":
+  sys.exit(main(sys.argv))
